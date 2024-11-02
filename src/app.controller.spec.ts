0f813a663f161ebb438718664a8dba63b04877ad
Franck <136079691+Freddyede@users.noreply.github.com>
2024-11-02 10:21:51 +0100

    add actions

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'test',
+          entities: [UserEntity],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([UserEntity]),
+      ],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
